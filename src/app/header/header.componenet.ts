a6600e4f62c4559434820d246b27ff46970ec95a
Pawel Stabryla <pawel_stabryla@epam.com>
2019-01-04 10:41:46 +0100

    init commit with project structure and initail design of header

---
+++ src/app/header/header.componenet.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.componenet.html'
+})
+
+export class HeaderComponent {
+  constructor(){
+  }
+}
