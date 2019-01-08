0b5f30992248fd5274ca718c5f08900083b92a7f
Pawel Stabryla <pawel_stabryla@epam.com>
2019-01-08 15:16:34 +0100

    add simple recipe selector and display details of selected recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeSelecter = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('TestRecipe', 'This is a test',
     'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/frying-pan-pizza-easy-recipe-collection.jpg'),
@@ -17,5 +19,9 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelecter(recipeEl: Recipe){
+    this.recipeSelecter.emit(recipeEl);
   }
 }
