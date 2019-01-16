0a6e185c9585c2910be1997c704e968feb38d515
stabri <paulstarbi@gmail.com>
2019-01-16 14:21:53 +0100

    usage of ngModel property binding and ngGroup

--- src/app/code-examples/form/form.component.ts
+++ src/app/code-examples/form/form.component.ts
@@ -9,7 +9,8 @@ import { NgForm } from '@angular/forms';
 export class FormComponent implements OnInit {
 
   @ViewChild('f')signupForm: NgForm;
-
+  defaultQuestion = 'pet';
+  answer = '';
   constructor() { }
 
   ngOnInit() {
