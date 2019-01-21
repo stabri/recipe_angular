d0834f9c5032298fedf1e06b320b0bb8dfeba059
stabri <paulstarbi@gmail.com>
2019-01-21 10:05:04 +0100

    forms fully work and finishd

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -54,6 +54,10 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['/recipes']);
   }
 
+  onDelete(index: number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImgPath = '';
