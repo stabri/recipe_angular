4d86e3f3ff8f3c04f751a9c2600580999262e4bb
stabri <paulstarbi@gmail.com>
2019-03-27 13:17:52 +0100

    extract recipes module, extract recipes -routing module with forChild, TODO/ sherd DropDownDirectives for all modules - can't be declera in more than one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,35 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'
 
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { CodeExamplesComponent } from './code-examples/code-examples.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { AuthGuard } from './app-route-guard.service';
-import { FormComponent } from './code-examples/form/form.component';
-import { FormReactiveComponent } from './code-examples/form-reactive/form-reactive.component';
-import { PipesComponent } from './code-examples/pipes/pipes.component';
-import { HttpComponent } from './code-examples/http/http.component';
-import { SinginComponent } from './auth/singin/singin.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {CodeExamplesComponent} from './code-examples/code-examples.component';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {AuthGuard} from './app-route-guard.service';
+import {FormComponent} from './code-examples/form/form.component';
+import {FormReactiveComponent} from './code-examples/form-reactive/form-reactive.component';
+import {PipesComponent} from './code-examples/pipes/pipes.component';
+import {HttpComponent} from './code-examples/http/http.component';
+import {SinginComponent} from './auth/singin/singin.component';
 
 const appRouts: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  { path: 'recipes', component: RecipesComponent, children: [
-    { path: '', component: RecipeStartComponent },
-    { path: 'new', component: RecipeEditComponent },
-    { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent },
-  ]},
-  { path: 'examples', component: CodeExamplesComponent, children: [
-    { path: 'form', component: FormComponent },
-    { path: 'form-reactive', component: FormReactiveComponent },
-    { path: 'pipes', component: PipesComponent },
-    { path: 'http', component: HttpComponent },
-  ]},
-  { path: 'examples/:option', canActivate: [AuthGuard], component: CodeExamplesComponent },
-  { path: 'shopping-list', component: ShoppingListComponent},
-  { path: 'singup', component: SinginComponent},
-  { path: '**', component: ErrorPageComponent},
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+  {
+    path: 'examples', component: CodeExamplesComponent, children: [
+      {path: 'form', component: FormComponent},
+      {path: 'form-reactive', component: FormReactiveComponent},
+      {path: 'pipes', component: PipesComponent},
+      {path: 'http', component: HttpComponent},
+    ]
+  },
+  {path: 'examples/:option', canActivate: [AuthGuard], component: CodeExamplesComponent},
+  {path: 'shopping-list', component: ShoppingListComponent},
+  {path: 'singup', component: SinginComponent},
+  {path: '**', component: ErrorPageComponent},
 
-]
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRouts)],
+  imports: [RouterModule.forRoot(appRouts)],  // forRoot only in app-routing module
   exports: [RouterModule],
 })
 export class AppRoutingModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,77 +1,68 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.componenet';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { BasicHighlightDirective } from './shared-directives/basic-highlight.directive';
-import { BetterHightlightsDirective } from './shared-directives/better-hightlights.directive';
-import { HightlightHostBindingDirective } from './shared-directives/hightlight-host-binding.directive';
-import { CodeExamplesComponent } from './code-examples/code-examples.component';
-import { UnlessDirective } from './shared-directives/structural-directives/unless.directive';
-import { DropDownDirective } from './shared-directives/structural-directives/dropdown.directive';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { AuthService } from './auth-fake.service';
-import { AuthGuard } from './app-route-guard.service';
-import { FormComponent } from './code-examples/form/form.component';
-import { FormReactiveComponent } from './code-examples/form-reactive/form-reactive.component';
-import { RecipeService } from './recipes/recipe.service';
-import { PipesComponent } from './code-examples/pipes/pipes.component';
-import { ShortenPipe } from './code-examples/pipes/shorten.pipe';
-import { FilterPipe } from './code-examples/pipes/filter.pipe';
-import { HttpComponent } from './code-examples/http/http.component';
-import { ServersService } from './servers.service';
-import { RecipePersistanceService } from './recipe-persistance.service';
-import { SinginComponent } from './auth/singin/singin.component';
-import { SingoutComponent } from './auth/singout/singout.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.componenet';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
+import {BasicHighlightDirective} from './shared-directives/basic-highlight.directive';
+import {BetterHightlightsDirective} from './shared-directives/better-hightlights.directive';
+import {HightlightHostBindingDirective} from './shared-directives/hightlight-host-binding.directive';
+import {CodeExamplesComponent} from './code-examples/code-examples.component';
+import {UnlessDirective} from './shared-directives/structural-directives/unless.directive';
+import {ShoppingListService} from './shopping-list/shopping-list.service';
+import {AppRoutingModule} from './app-routing.module';
+import {ErrorPageComponent} from './error-page/error-page.component';
+import {AuthService} from './auth-fake.service';
+import {AuthGuard} from './app-route-guard.service';
+import {FormComponent} from './code-examples/form/form.component';
+import {FormReactiveComponent} from './code-examples/form-reactive/form-reactive.component';
+import {RecipeService} from './recipes/recipe.service';
+import {PipesComponent} from './code-examples/pipes/pipes.component';
+import {ShortenPipe} from './code-examples/pipes/shorten.pipe';
+import {FilterPipe} from './code-examples/pipes/filter.pipe';
+import {HttpComponent} from './code-examples/http/http.component';
+import {ServersService} from './servers.service';
+import {RecipePersistanceService} from './recipe-persistance.service';
+import {SinginComponent} from './auth/singin/singin.component';
+import {SingoutComponent} from './auth/singout/singout.component';
+import {RecipesModule} from "./recipes/recipes.module";
+import {DropDownDirective} from "./shared-directives/structural-directives/dropdown.directive";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeDetailComponent,
-    BasicHighlightDirective,
-    BetterHightlightsDirective,
-    HightlightHostBindingDirective,
-    CodeExamplesComponent,
-    UnlessDirective,
-    DropDownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
     ErrorPageComponent,
     FormComponent,
     FormReactiveComponent,
     PipesComponent,
-    ShortenPipe,
-    FilterPipe,
     HttpComponent,
     SinginComponent,
     SingoutComponent,
+    CodeExamplesComponent,
+    BasicHighlightDirective,
+    UnlessDirective,
+    BetterHightlightsDirective,
+    HightlightHostBindingDirective,
+    DropDownDirective,
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
+    RecipesModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [ShoppingListService, AuthService, AuthGuard, RecipeService, ServersService, RecipePersistanceService],
   bootstrap: [AppComponent]
-})
-export class AppModule { }
+ })
+export class AppModule {
+}
